b93b950f93b7a870b9c88ef0b7b2556bd9d2a94b
TheStrechh <TheStrechh@gmail.com>
2023-07-01 02:42:19 +0530

    redwood: set selinux permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=0
 BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Kernel modules
 BOOT_KERNEL_MODULES := \
