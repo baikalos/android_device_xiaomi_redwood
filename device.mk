195c10f47ca1aefb240805b0dfc83642e7e227c7
Bruno Martins <bgcngm@gmail.com>
2023-07-22 23:44:16 -0300

    redwood: Migrate to AIDL ClearKey DRM HAL Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -175,7 +175,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # Fastbootd
