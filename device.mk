a5c3d9c3eacbe9c650f9b646e313feb383ef0d83
Bruno Martins <bgcngm@gmail.com>
2023-07-28 20:37:08 -0300

    redwood: build android.hardware.drm@1.4.vendor Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -175,7 +175,8 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
