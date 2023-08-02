b3f12920958ea0afefa9861aece90c12a71dc800
TheStrechh <TheStrechh@gmail.com>
2023-08-02 05:59:04 +0530

    redwood: Enable blur

--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -32,3 +32,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := POCO/redwood_global/redwood:13/RKQ1.211001.001/V14.0.2.0.TMSMIXM:user/release-keys
+
+# Blur
+TARGET_ENABLE_BLUR := true
